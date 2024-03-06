4d83c2211e212f967ae3839ef0bc398a2aed4bbd
Ali Mansour <alialhadimansour21@gmail.com>
2024-03-06 11:37:36 +0200

    Add & Delete  a restaurant

--- scripts/admin/resturants.js
+++ scripts/admin/resturants.js
@@ -1,11 +1,93 @@
 const createResturant = document.getElementById("add-resturant");
 const popUp = document.getElementById("pop-up");
 const closePopUp = document.getElementById("close");
+const resturantCreateForm = document.getElementById("restaurantForm");
+const restaurantCards = document.getElementById("resturants");
+const nameValue = document.getElementById("name");
+const descriptionValue = document.getElementById("description");
+const fileValue = document.getElementById("file");
+const categoryValue = document.getElementById("category");
+const deleteButtons = document.querySelectorAll(".remove");
+let resturantId = 0;
+let resturants = [];
 function showPopup() {
   popUp.classList.add("show");
 }
 function close() {
   popUp.classList.remove("show");
 }
+function addToLocalStorage() {
+  localStorage.setItem("restaurants", JSON.stringify(resturants));
+}
+function createResturantAction() {
+  const resturantObject = {
+    id: resturantId,
+    name: nameValue.value,
+    description: descriptionValue.value,
+    image: fileValue.files.length > 0 ? fileValue.files[0].name : "",
+    category: categoryValue.value,
+    favirote: 0,
+  };
+  resturants.push(resturantObject);
+  resturantId++;
+  addToLocalStorage();
+}
+function getRestaurantsFromLocalStorage() {
+  const restaurantsString = localStorage.getItem("restaurants");
+  if (restaurantsString) {
+    resturants = JSON.parse(restaurantsString);
+    resturantId = resturants.length;
+  }
+}
+function restaurantLoader() {
+  restaurantCards.innerHTML = "";
+  if (resturants.length > 0) {
+    for (let index = 0; index < resturants.length; index++) {
+      const restaurant = resturants[index];
+      generateRestaurantCard(restaurant);
+    }
+  }
+}
+function generateRestaurantCard(restaurant) {
+  const card = document.createElement("div");
+  const restaurantId = restaurant.id;
+
+  card.classList.add("card");
+  card.classList.add("bg-white");
+  card.classList.add("rad-6");
+  card.classList.add("p-relative");
+  card.innerHTML = `<img class="cover" src="../../assets/${restaurant.image}" alt="" />
+            <div class="p-20 content">
+              <h4 class="m-0">${restaurant.name}</h4>
+              <p class="description c-gray mt-15 fs-14">
+                ${restaurant.description}
+              </p>
+            </div>
+            <div class="info p-15 p-relative between-flex">
+              <button  data-set-id="${restaurantId}" class="title bg-red c-white b-none btn-shape remove">Remove</button>
+              <span class="c-gray"> Favirotes: ${restaurant.favirote} </span>
+              <span class="c-gray"> 120 </span>
+            </div>`;
+
+  restaurantCards.appendChild(card);
+}
+function deleteRestaurant(e) {
+  const deleteButton = e.target;
+  const selectedRestaurantId = deleteButton.dataset.restaurantId;
+  resturants = resturants.filter(
+    (restaurant) => restaurant.id !== parseInt(selectedRestaurantId)
+  );
+  addToLocalStorage();
+  restaurantLoader();
+}
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", deleteRestaurant);
+});
+restaurantCards.addEventListener("click", deleteRestaurant);
+resturantCreateForm.addEventListener("submit", createResturantAction);
 createResturant.addEventListener("click", showPopup);
 closePopUp.addEventListener("click", close);
+getRestaurantsFromLocalStorage();
+window.addEventListener("load", () => {
+  restaurantLoader();
+});
--- scripts/admin/user.js
+++ scripts/admin/user.js
@@ -7,7 +7,6 @@ const userNameValue = document.getElementById("username");
 const passwordValue = document.getElementById("password");
 const userCards = document.getElementById("users-page");
 const userForm = document.getElementById("userForm");
-const removeUser = document.getElementById("removeUser");
 let allUsers = [];
 let userId = 0;
 function showPopup() {
@@ -37,7 +36,6 @@ function getUsersFromLocalStorage() {
     allUsers = JSON.parse(usersString);
     userId = allUsers.length;
   }
-  console.log(allUsers);
 }
 function userLoader() {
   userCards.innerHTML = "";
@@ -46,12 +44,9 @@ function userLoader() {
       const user = allUsers[index];
       generateUserCard(user);
     }
-  } else {
-    console.log("no");
   }
 }
 function generateUserCard(user) {
-  console.log(user.id);
   const card = document.createElement("div");
   const userId = user.id;
   card.classList.add("user");
@@ -80,26 +75,23 @@ function generateUserCard(user) {
                 Remove
               </button>
             </div>`;
+
   userCards.appendChild(card);
 }
 function deleteUser(e) {
   const deleteButton = e.target.closest(".remove");
   if (deleteButton) {
     const selectedUserId = deleteButton.dataset.userId;
-    console.log(selectedUserId);
-    console.log("Before deletion:", allUsers); // Log the array before deletion
-
     allUsers = allUsers.filter((user) => user.id !== parseInt(selectedUserId));
     addToLocalStorage();
     userLoader();
-    console.log("After deletion:", allUsers); // Log the array before deletion
   }
 }
 userForm.addEventListener("submit", createUserAction);
 createUser.addEventListener("click", showPopup);
 closePopUp.addEventListener("click", close);
+userCards.addEventListener("click", deleteUser);
+getUsersFromLocalStorage();
 window.addEventListener("load", function () {
   userLoader();
 });
-userCards.addEventListener("click", deleteUser);
-getUsersFromLocalStorage();
